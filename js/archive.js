c07f87f9d30fe64724c4f9e337ba916036c9dc0c
Mai Nguyễn Anh Minh <mainguyenanhminh1996@gmail.com>
2022-11-13 21:33:51 +0100

    fix: correct tags generated link

--- js/archive.js
+++ js/archive.js
@@ -122,7 +122,8 @@ https://github.com/kitian616/jekyll-TeXt-theme
         if (_tag === '' || typeof _tag !== 'string') {
           setUrlQuery();
         } else {
-          setUrlQuery('.html?tag=' + _tag);
+          // setUrlQuery('.html?tag=' + _tag);
+          setUrlQuery('?tag=' + _tag);
         }
       } else {
         buttonFocus(searchButtonsByTag(tag));
